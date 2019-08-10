52306225ca18ef376a3ff0a947b1551e11281d2e
Alexey_Lisenko <detoner777@gmail.com>
2019-08-10 12:27:20 +0300

    Setapp MonoDB and mongoose

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(
 	new GoogleStategy({
@@ -9,9 +12,17 @@ passport.use(
 		callbackURL: '/auth/google/callback'
 	},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('access token', accessToken);
-			console.log('refresh token', refreshToken);
-			console.log('profile', profile);
+			User.findOne({ googleId: profile.id })
+				.then(existingUser => {
+					if (existingUser) {
+						done(null, existingUser);
+					} else {
+
+						new User({ googleId: profile.id })
+							.save()
+							.then(user => done(null, user));
+					}
+				});
 		}
 	)
 );
