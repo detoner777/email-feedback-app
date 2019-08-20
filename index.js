e67350d3739b8087004c875ddd92491d23614a54
Alexey_Lisenko <detoner777@gmail.com>
2019-08-20 11:46:52 +0300

    heroku debbag #2

--- index.js
+++ index.js
@@ -22,23 +22,21 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRouthes')(app);
+require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-	// express will serve up production assets
+	// Express will serve up production assets
 	// like our main.js file, or main.css file!
 	app.use(express.static('client/build'));
 
-	// express will serve up the index.html File
-	// if it doesn`t recognize the route
+	// Express will serve up the index.html file
+	// if it doesn't recognize the route
 	const path = require('path');
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
 	});
 }
 
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT); 
+app.listen(PORT);
